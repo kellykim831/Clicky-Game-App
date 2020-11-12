50268cff0291a895dae2b552760d6f0cb9254810
Kelly Kim <kellykim408@gmail.com>
2020-11-11 23:01:50 -0800

    update app.js

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,39 +1,75 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
+import React, { Component } from 'react';
+import Cards from './southpark.json';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CharacterList from './components/CharacterList';
+
+//set state to 0
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
-  state = {
-    friends: friends
+    state = {
+      Cards,
+      clickedCard: [],
+      score: 0,
+      highScore: 0,
+      isGuessed: false
+    };
+  
+    randomGenerator = (a, b) => (Math.random() > 0.5 ? -1 : 1);
+//onclcick for images
+imageClick = id => {
+    const currentCard = id;
+    const CardAlreadyClicked =
+      this.state.clickedCard.indexOf(currentCard) > -1;
+
+    if (CardAlreadyClicked) {
+      this.setState({
+        Cards: this.state.cards.sort(this.randomGenerator),
+        clickedCard: [],
+        score: 0,
+        highScore: 0,
+        isGuessed: false
+      });
+      alert('You lose. Play again?');
+    } else {
+      let score = this.state.score;
+      let cards = this.state.cards;
+
+      this.setState(
+        {
+          cards: this.state.cards.sort(this.randomGenerator),
+          clickedCard: this.state.clickedcard.concat(currentCard),
+          score: score + 1,
+          highScore: Math.max(this.state.highScore, score),
+          isGuessed: true
+        },
+        () => {
+          if (this.state.score === cards.length) {
+            alert('Yay! You Win!');
+            this.setState({
+              cards: this.state.cards.sort(this.randomGenerator),
+              clickedCard: [],
+              score: 0,
+              highScore: 0
+            });
+          }
+        }
+      );
+    }
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends: friends });
-  };
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
+    const { cards, score, highScore, isGuessed } = this.state;
+
     return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
+      <div className='application'>
+        <Header score={score} highScore={highScore} isGuessed={isGuessed} />
+        <div className='wrapper'>
+          <CharacterList cards={cards} imageClick={this.imageClick} />
+        </div>
+        <Footer />
+      </div>
     );
   }
 }
